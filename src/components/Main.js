9c746932ea2fc9fc9f84c41c017069d4abaacdbc
smk-romote <2685331701@qq.com>
2016-12-18 22:27:44 +0800

    add data

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,32 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/App.scss');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+var imageDatas=require('../stores/imageDatas.json');
+
+imageDatas=(function genImageURL(imageDatasArr){
+	for(var i=0,j=imageDatasArr.length;i<j;i++){
+		var singleImageData=imageDatasArr[i];
+		singleImageData.imageURL=require('../images/'+singleImageData.fileName);
+		imageDatasArr[i]=singleImageData;
+	}
+	return imageDatasArr;
+})(imageDatas);
+
+//let yeomanImage = require('../images/yeoman.png');
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+      // <div className="index">
+      //   <img src={yeomanImage} alt="Yeoman Generator" />
+      //   <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+      // </div>
+      <section className="stage">
+      	<section className="img-sec"></section>
+      	<nav className="controller-nav"></nav>
+      </section>
     );
   }
 }
